577d9bec6667e51dc63ce745893f9b86d599e722
Baqaullah Soomro <wafaullahattaullahsoomro@gmail.com>
2024-03-21 03:24:36 +0500

    push my calculator  to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# npx7-calculator" 
---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter first number:", type: "number", name: "firstnumber"
+    },
+    { message: "enter second number: ", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operator to perform opression",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    },
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
